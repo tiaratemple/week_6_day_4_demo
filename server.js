39d7e01b445c5933ba078c84764ee5a7c940ccf3
Tiara Temple <tmtemple7@gmail.com>
2022-10-20 11:10:59 -0500

    ready to deploy

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+
+app.use(express.json())
+app. use(cors())
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '52360bdf17b54d0692c0b4beef2dfdac',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    rollbar.info("a request for all students was made")
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+            rollbar.error("someone tried entering an empty name")
+           res.status(400).send('You must enter a name.')
+       } else {
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    res.status(200).send(students)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
